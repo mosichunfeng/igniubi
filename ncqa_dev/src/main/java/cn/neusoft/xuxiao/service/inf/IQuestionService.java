095bdd3dbab35d3cb0b9cd53c3e67dee242964e6
何佩 <1271729489@qq.com>
2018-11-14 21:06:53 +0800

    111

--- ncqa_dev/src/main/java/cn/neusoft/xuxiao/service/inf/IQuestionService.java
+++ ncqa_dev/src/main/java/cn/neusoft/xuxiao/service/inf/IQuestionService.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import cn.neusoft.xuxiao.dao.entity.Question;
 import cn.neusoft.xuxiao.dao.entity.QuestionBase;
 
 public abstract interface IQuestionService {
 	public abstract void insertDataToBase(MultipartFile paramMultipartFile);
 	
 	public abstract List<QuestionBase> getAllQuestionBase();
+	
+	public abstract List<Question> getAllQuestionByBaseId();
 }
